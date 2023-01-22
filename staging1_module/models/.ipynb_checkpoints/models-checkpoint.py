740deb80b196c3c42ec25fff42d7908d669eee38
Reem-Elfalah <r.fellah@yahoo.com>
2023-01-22 10:18:34 +0000

    staging1_module

---
+++ staging1_module/models/.ipynb_checkpoints/models-checkpoint.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class staging1_module(models.Model):
+#     _name = 'staging1_module.staging1_module'
+#     _description = 'staging1_module.staging1_module'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
